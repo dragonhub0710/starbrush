b774c0aca217fb698208bc980d144809540da2c3
dragonhub0710 <roberttomson0803@gmail.com>
2024-03-14 01:16:11 +0200

    updated model from dall-e to stable diffusion

--- backend/controllers/image.controller.js
+++ backend/controllers/image.controller.js
@@ -1,23 +1,45 @@
 const OpenAI = require("openai");
 const axios = require("axios");
+const Replicate = require("replicate");
+
 require("dotenv").config();
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
 });
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
 
 async function generateImage(prompt, base_prompt) {
   try {
     const image_prompt = base_prompt + `Prompt: ${prompt}`;
-    return openai.images
-      .generate({
-        model: "dall-e-3",
-        prompt: image_prompt,
-        n: 1,
-        size: "1024x1024",
-        quality: "standard",
-      })
-      .then((response) => response.data[0].url);
+    return replicate
+      .run(
+        "playgroundai/playground-v2-1024px-aesthetic:42fe626e41cc811eaf02c94b892774839268ce1994ea778eba97103fe1ef51b8",
+        {
+          input: {
+            width: 1024,
+            height: 1024,
+            prompt: image_prompt,
+            scheduler: "K_EULER_ANCESTRAL",
+            guidance_scale: 3,
+            apply_watermark: false,
+            negative_prompt: "",
+            num_inference_steps: 50,
+          },
+        }
+      )
+      .then((res) => res[0]);
+    // return openai.images
+    //   .generate({
+    //     model: "dall-e-3",
+    //     prompt: image_prompt,
+    //     n: 1,
+    //     size: "1024x1024",
+    //     quality: "standard",
+    //   })
+    //   .then((response) => response.data[0].url);
   } catch (err) {
     console.log(err);
   }
@@ -47,15 +69,15 @@ const imgGenerator = async (req, res) => {
     });
 
     if (res_image.data.data.prompt) {
-      const firstBatchPrompts = new Array(7).fill(
+      const imagePrompts = new Array(10).fill(
         completion.choices[0].message.content
       );
-      const firstBatchPromises = firstBatchPrompts.map((prompt) =>
+      const imagePromises = imagePrompts.map((prompt) =>
         generateImage(prompt, res_image.data.data.prompt)
       );
-      const firstBatchUrls = await Promise.all(firstBatchPromises);
+      const imageUrls = await Promise.all(imagePromises);
 
-      res.json(firstBatchUrls);
+      res.json(imageUrls);
     }
   } catch (err) {
     console.error(err.message);
